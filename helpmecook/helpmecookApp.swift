75a68df84bc9e1cfdde9bf54bcb6a169b706cdda
Rafael Badaro <rafabadaro.15@gmail.com>
2024-08-02 19:42:09 -0300

    initial commit

---
+++ helpmecook/helpmecookApp.swift
@@ -0,0 +1,17 @@
+//
+//  helpmecookApp.swift
+//  helpmecook
+//
+//  Created by Rafael Badaró on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct helpmecookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
